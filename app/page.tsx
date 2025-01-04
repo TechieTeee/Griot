7ede2902b68419d471df4e8e0642885e7b8962e9
TechieTeee <techieteee@protonmail.com>
2025-01-04 22:07:15 +0000

    Updated style elements - pattern and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -38,9 +38,9 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask AI</button>
+      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask Griot</button>
       {
-        loading && <p>Asking AI ...</p>
+        loading && <p>Asking Griot ...</p>
       }
       {
         result && <p>{result}</p>
